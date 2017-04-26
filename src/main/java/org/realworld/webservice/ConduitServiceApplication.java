3d608a0f854eeaf44b37eb8913f36e6ee44edba9
Cameron Chapman <cameron.c.chapman@gmail.com>
2017-04-25 23:19:58 -0500

    Adding basic project structure.

---
+++ src/main/java/org/realworld/webservice/ConduitServiceApplication.java
@@ -0,0 +1,12 @@
+package org.realworld.webservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConduitServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConduitServiceApplication.class, args);
+	}
+}
