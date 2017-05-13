a084e04a6bd38fc41b5b140922844642e66bd923
Admir Sabanovic <admir_sabanovic@live.com>
2017-05-13 18:26:08 +0200

    changed hardcoded values to jdbc read

--- src/main/java/org/realworld/webservice/security/JwtUserDetailsServiceImpl.java
+++ src/main/java/org/realworld/webservice/security/JwtUserDetailsServiceImpl.java
@@ -1,29 +1,31 @@
 package org.realworld.webservice.security;
 
+import org.realworld.webservice.data.UserDao;
 import org.realworld.webservice.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Component;
 
+import java.util.Optional;
+
 @Component
 public class JwtUserDetailsServiceImpl implements UserDetailsService {
 
+    @Autowired
+    private UserDao userDao;
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-
-        User user = new User();
-        user.setId(new Long(9));
-        user.setUsername("testing this jwt wiring");
-        user.setEmail("jwtisworking@cool.com");
-        user.setPassword("$2y$10$/6v2u1GupcKz0W0Ys0IVMeufeJu7DGS/tkHGu1cr9tOLG4b0pikUy");
-
-
-        if (user == null) {
-            throw new UsernameNotFoundException(String.format("No user found with username '%s'.", username));
-        } else {
-            return JwtUserFactory.create(user);
-        }
+        User user = this.userDao.getUserByUsername(username);
+        return Optional.ofNullable(user)
+                .map(JwtUserFactory::create)
+                .orElseThrow(() ->
+                        new UsernameNotFoundException(
+                                String.format("No user found with username '%s'.", username)
+                        )
+                );
     }
 
 }
